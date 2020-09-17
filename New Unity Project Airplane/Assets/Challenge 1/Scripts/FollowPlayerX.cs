6fdbd3b93611205a38512f01291c33176851975d
Triannon Kreutzer <10920499@my.uvu.edu>
2020-09-16 20:28:47 -0600

    Started fixing bugs in airplane simulator

---
+++ New Unity Project Airplane/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    // Sets the target for the main camera to follow
+    public GameObject plane;
+    private Vector3 offset = new Vector3(40,0,10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Makes the main camera follow the players position
+        transform.position = plane.transform.position + offset;
+    }
+}
