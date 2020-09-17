f8aeffabcebb2cfae306a445e722a2b0d597f79f
Triannon Kreutzer <10920499@my.uvu.edu>
2020-09-16 21:53:21 -0600

    Edited code to make plane move forward at constant rate

--- New Unity Project Airplane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ New Unity Project Airplane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -24,9 +24,11 @@ public class PlayerControllerX : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.up * Time.deltaTime * horizontalInput * rotationSpeed);
+
+       
     }
 }
