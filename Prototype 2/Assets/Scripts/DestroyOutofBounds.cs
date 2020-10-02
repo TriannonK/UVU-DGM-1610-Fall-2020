89d1fe6dcc347b5c3b2f9e35c070315b9017a554
Triannon Kreutzer <10920499@my.uvu.edu>
2020-10-02 10:30:38 -0600

    2.3 Animal Spawn System

--- Prototype 2/Assets/Scripts/DestroyOutofBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutofBounds.cs
@@ -6,19 +6,17 @@ public class DestroyOutofBounds : MonoBehaviour
 {
     public float topBounds = 30f;
     public float lowerbounds = -10f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
+        // Destroy any Object entering the topbounds
         if(transform.position.z > topBounds)
         {
             Destroy(gameObject);
         }
+        // Destroy any Objects entering the lowerbounds
         else if(transform.position.z < lowerbounds)
         {
             Destroy(gameObject);
