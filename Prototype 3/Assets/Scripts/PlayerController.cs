8bfe2efdc0f2681ebf920f34f2da330ebc424f4b
Triannon Kreutzer <10920499@my.uvu.edu>
2020-10-14 17:32:49 -0600

    Created player controller, obstacle, and spawn manager for prototype 3

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float jumpForce;
+    public float gravityMod;
+
+    public bool isGrounded = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)&& isGrounded == true)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        isGrounded = true;
+    }
+}
